1b27c73be8c496d84ae6d938bd86b79f3d1dae0a
Derek Crosson <derekcrosson18@gmail.com>
2022-08-11 04:16:54 +0200

    fix: correct access token config

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,5 @@ terraform {
   backend "gcs" {
     bucket  = "anya-final-project-terraform-state"
     prefix  = "terraform/state"
-
-    #credentials = "/home/runner/work/anya-final-project/anya-final-project/gha-creds-*.json"
   }
 }
