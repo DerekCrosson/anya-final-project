4413f53f7bd0f45947d522e6ed6266472963f4f0
Derek Crosson <derekcrosson18@gmail.com>
2022-08-11 04:45:24 +0200

    chore: turn off pipeline - fix later

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,5 +2,7 @@ terraform {
   backend "gcs" {
     bucket  = "anya-final-project-terraform-state"
     prefix  = "terraform/state"
+
+    credentials = "/home/runner/work/anya-final-project/anya-final-project/gha-creds-*.json"
   }
 }
