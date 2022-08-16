939ea633ec68891a30907185fc2d7e94f63383fd
Derek Crosson <derekcrosson18@gmail.com>
2022-08-16 09:52:58 +0200

    refactor: change ssh key format

--- terraform/modules/nodes/variables.tf
+++ terraform/modules/nodes/variables.tf
@@ -3,7 +3,7 @@ variable "ssh_user" {
 }
 
 variable "ssh_public_key" {
-  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC4JIkMRmzZwH4PEx4RRikIUQp5Hcvv1Ys+UZY9I33JufLsZtn/KZsRx0zQBNPA6258jepFSUFsNu8CK/g4LfGld2K/c2YE15x9R5aihn5fy3YGYsnBiDtO/kRAzuqo4c03y2Uf9y86c8IRXCGfev5RnxCLpnPuVmeFmq1jbqu2TgoMDyMplbdzjfS8/VPN4gz2odl+g1R4G5BPO30fYAEYD74828yw7jEmQtYHHLqhLJyQ9mXBga0iE2ZYPZgWUqr6+SftjWX4ALQwnWToFUaiyDxwzr6QB/QtmfBLaqdSaW45vYxFxh9q6Qi3McrSaRNpxI/jIFyMEdbN+jv40U38U5vwABGjq8Zu6AwhsZqi3b7ny7yiWRiEXh1lAK+9qY27X8y/T1QsSK6CyVaM2yyM4Nrl4vpnQZ8G/DGTRBihauUYU94tbryQTdcDgmj9Ve9/wUK/dkenrRFDIdp/iPO9sXCSztQQhZSnkKvoI7TNOv9qMUk1sbr3+B75GWmgTuM="
+  default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDx5iIXV4UioEK/nmRnI1q84DXsrMaU9HInmFEM4fOky terraform@anya-fellowship-project.iam.gserviceaccount.com"
 }
 
 variable "volumes" {
