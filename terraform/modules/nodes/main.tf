ca10a036a177fb4c0242c74353c7e577c596a624
Derek Crosson <derekcrosson18@gmail.com>
2022-08-16 08:24:15 +0200

    fix: remove conflicting configuration

--- terraform/modules/nodes/main.tf
+++ terraform/modules/nodes/main.tf
@@ -79,7 +79,6 @@ resource "google_compute_firewall" "ssh" {
   network       = google_compute_network.vpc_network.name
   priority      = "${ var.node_ssh_firewall_rule_priority }"
   source_service_accounts = ["${ google_compute_instance.node.*.service_account.email }"]
-  target_tags   = ["allow-ssh"]
 }
 
 resource "google_compute_firewall" "blockchain" {
